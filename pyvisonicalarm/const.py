3c49da635117e245d8a2338dce72f424bc7e1e12
Mark Parker <msparker@sky.com>
2023-05-04 16:17:19 +0100

    v0.1.0b2

--- pyvisonicalarm/const.py
+++ pyvisonicalarm/const.py
@@ -1,4 +1,4 @@
-__VERSION__ = "0.1.0b1"
+__VERSION__ = "0.1.0b2"
 
 import enum
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyvisonicalarm",
-    version="0.1.0b1",
+    version="0.1.0b2",
     author="Mark Parker",
     author_email="msparker@sky.com",
     description="A simple library for the Visonic Alarm API written in Python 3",
